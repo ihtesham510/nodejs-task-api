30098f974af77f2c0b651a8bd17cda5d4124dcb9
ihtesham ul haq <ihteshamulhaq510@gmail.com>
2024-01-04 17:54:18 +0500

    added all the packages and imported them

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+const uri = process.env.MONGO_URI || "mongodb://localhost:27017/";
+connect(uri);
+
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send("Yo!");
+});
+
+async function connect(uri) {
+  await mongoose.connect(uri);
+  app.listen(port, console.log(`App is running on PORT : ${port}`));
+}
+
